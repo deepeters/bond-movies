5c2a61b0605370b0cf264c48ca6463114991c349
deepeters <53930966+deepeters@users.noreply.github.com>
2021-12-14 20:36:11 +0300

    Associate database objects with code objects using a connection string.

--- Services/Data/SecurityDAO.cs
+++ Services/Data/SecurityDAO.cs
@@ -1,6 +1,7 @@
 ﻿using BondMovies.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -8,16 +9,31 @@ namespace BondMovies.Services.Data
 {
     public class SecurityDAO
     {
+
+        //connect to the database
+        string connectionString = "Data Source=(localdb)\ProjectsV13;Initial Catalog=BondMovies;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
         internal bool FindByUser(UserModel user)
         {
-            if (user.Username == "Admin" && user.Password == "Secret")
+            //start by assuming that nothing is found in the query
+            bool success = false;
+
+            //write the sql expression
+            string queryString = "SELECT * FROM dbo.Users WHERE username = @Username AND password = @Password";
+
+            //create and open the connection to the database inside a using block.
+
+            //Ensures that all resource are closed properly when the query is done.
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                return true;
+                //create the command and parameter objects
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                command.Parameters.Add("@Username", System.Data.SqlDbType.VarChar, 50).Value = user.Username;
+                command.Parameters.Add("@Password", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
             }
-            else
-            {
-                return false;
-            }
+
             //throw new NotImplementedException();
         }
     }
