cdfbd5e559b951755020ab4b53f2222dedb56f05
deepeters <53930966+deepeters@users.noreply.github.com>
2021-12-14 08:26:17 +0300

    Develop a log in form to accept user input.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using BondMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BondMovies.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public string Login(UserModel userModel)
+        {
+            return "Results: Username = " + userModel.Username + "PW = " + userModel.Password;
+        }
+
+    }
+}
