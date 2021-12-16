ae59e0419741519a6780c8f6dbbda0fb35b2465d
deepeters <53930966+deepeters@users.noreply.github.com>
2021-12-16 10:14:31 +0300

    Build a database with James Bond's gadgets and their description.

---
+++ Controllers/GadgetsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BondMovies.Controllers
+{
+    public class GadgetsController : Controller
+    {
+        // GET: Gadgets
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/GadgetModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BondMovies.Models
+{
+    public class GadgetModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AppersIn { get; set; }
+        public string WithThisActor { get; set; }
+         
+        public GadgetModel()
+        {
+            Id = -1;
+            Name = "Nameless";
+            Description = "Nothing";
+            AppersIn = "Nowhere";
+            WithThisActor = "Noone";
+        }
+
+        public GadgetModel(int id, string name, string description, string appersIn, string withThisActor)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            AppersIn = appersIn;
+            WithThisActor = withThisActor;
+        }
+    }
+}
