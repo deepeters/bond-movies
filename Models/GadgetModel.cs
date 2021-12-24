a3a30618cecf6b081bac90e267051de2d636226b
deepeters <53930966+deepeters@users.noreply.github.com>
2021-12-24 10:07:30 +0300

    Fix bugs and issues

--- Models/GadgetModel.cs
+++ Models/GadgetModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +10,15 @@ namespace BondMovies.Models
     public class GadgetModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
+        [DisplayName ("Appears in this Movie")]
         public string AppersIn { get; set; }
+        [Required]
+        [DisplayName("James Bond is Plyaed by")]
         public string WithThisActor { get; set; }
          
         public GadgetModel()
